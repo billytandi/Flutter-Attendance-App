e43fc8fe43e41842eb467b7bd40cbc23e74086b9
Billy Tandias <57698020+billytandi@users.noreply.github.com>
2024-11-23 11:56:01 +0700

    Update qrscanpage.dart

--- lib/views/qrscanpage.dart
+++ lib/views/qrscanpage.dart
@@ -56,6 +56,9 @@ class _QRScanPageState extends State<QRScanPage> {
       double qrLat = -6.112721;
       double qrLon = 106.881920;
 
+      double kampusLat = -6.316056;
+      double kampusLon = 106.79497;
+
       // Get UID
       User? user = FirebaseAuth.instance.currentUser;
 
